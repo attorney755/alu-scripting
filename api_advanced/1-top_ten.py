97c1002f7b331ae6216fc48cb4f217c3463987fb
Attorney Valois <v.niyigaba@alustudent.com>
2025-07-07 11:33:31 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,15 +2,27 @@
 """ top_ten.py """
 import requests
 
-
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    """ Prints the titles of the first 10 hot posts listed in a subreddit """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     headers = {'User-Agent': 'Mozilla/5.0'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    try:
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+    except Exception:
+        print(None)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
