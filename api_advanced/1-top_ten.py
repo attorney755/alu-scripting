9d848882f49509f24b623536d30ee411c389a0c0
Attorney Valois <v.niyigaba@alustudent.com>
2025-07-07 12:07:54 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
+"""
+Script that fetches 10 hot posts for a given subreddit.
+It prints the titles of the first 10 hot posts listed for a given subreddit.
+If the subreddit is invalid or no posts are found, it prints None.
+"""
+
 import requests
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid or no posts are found, it prints None.
+    """
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     subreddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
@@ -13,7 +21,11 @@ def top_ten(subreddit):
             json_data = response.json()
             posts = json_data.get('data', {}).get('children', [])
 
-            for post in posts[:10]:  # Ensure we only take up to 10 posts
+            if not posts:
+                print(None)
+                return
+
+            for post in posts[:10]:
                 print(post.get('data', {}).get('title'))
         else:
             print(None)
