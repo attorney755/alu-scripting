7d95a270274e92cdcafea5c1636b764cb5d3e1fa
Attorney Valois <v.niyigaba@alustudent.com>
2025-07-07 12:05:53 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that fetches 10 hot posts for a given subreddit."""
 import requests
 
-
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    headers = {'User-Agent': 'myfirst/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    try:
+        response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            json_data = response.json()
+            posts = json_data.get('data', {}).get('children', [])
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+            for post in posts[:10]:  # Ensure we only take up to 10 posts
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
+    except requests.exceptions.RequestException:
         print(None)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
