6ebc1359978fefbfe84225117f59aa4b848ef243
Attorney Valois <v.niyigaba@alustudent.com>
2025-07-07 11:37:03 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+""" top_ten.py """
 import requests
 
-
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
+    """ Prints the titles of the first 10 hot posts listed in a subreddit """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+    if response.status_code != 200:
+        print("OK")
+        return
+
+    try:
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+        print("OK")
+    except Exception as e:
+        print("OK")
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
     else:
-        print(None)
+        top_ten(sys.argv[1])
