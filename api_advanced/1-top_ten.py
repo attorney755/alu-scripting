b3008edd47c46ad5e8fd628ef579e7eb46ef4f31
Attorney Valois <v.niyigaba@alustudent.com>
2025-07-07 11:34:49 +0200

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
+
 def top_ten(subreddit):
-    """ Prints the titles of the first 10 hot posts listed in a subreddit """
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
-    if response.status_code != 200:
-        print(None)
-        return
-
-    try:
-        posts = response.json().get('data', {}).get('children', [])
-        for post in posts:
-            print(post['data']['title'])
-    except Exception:
-        print(None)
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
-        top_ten(sys.argv[1])
+        print(None)
